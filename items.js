eafee46ba941cc2a4e6b6da9255d930cab52be47
sivanaidupattapu <adisivanaidup@gmail.com>
2024-04-26 16:40:47 +0530

    hi

---
+++ items.js
@@ -0,0 +1,140 @@
+var items = {
+
+    Desserts: [
+        {
+            itemname: 'cake',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 900
+        },
+        {
+            itemname: 'Gulab jamun',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 1800
+        },
+        {
+            itemname: 'Sandesh',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 1100
+        },
+        {
+            itemname: 'Kheer',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 1200
+        }
+    ],
+
+    starters: [
+        {
+            itemname: 'Panner-manchuria',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 1999
+        },
+        {
+            itemname: 'Chicken-manchuria',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 1899
+        },
+        {
+            itemname: 'veg-manchuria',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 990
+        },
+        {
+            itemname: 'Chicken-lolipops',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 2499
+        }
+    ],
+
+    liquids: [
+        {
+            itemname: 'Sambar',
+            Quantity: 1,
+            Quantityunits: 'liter',
+            price: 399
+        },
+        {
+            itemname: 'Curd-Chutney',
+            Quantity: 1,
+            Quantityunits: 'liter',
+            price: 199
+        },
+        {
+            itemname: 'Rasam',
+            Quantity: 1,
+            Quantityunits: 'liter',
+            price: 290
+        },
+        {
+            itemname: 'Pachipullusu',
+            Quantity: 1,
+            Quantityunits: 'liter',
+            price: 299
+        }
+    ],
+
+    Rice: [
+        {
+            itemname: 'white-rice',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 249
+        },
+        {
+            itemname: 'tomato-rice',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 299
+        },
+        {
+            itemname: 'veg-biriyani',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 499
+        },
+        {
+            itemname: 'Chicken-biriyani',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 999
+        }
+    ],
+
+    Curries: [
+        {
+            itemname: 'Panner',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 1499
+        },
+        {
+            itemname: 'Chicken',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 999
+        },
+        {
+            itemname: 'tomato-dal',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 599
+        },
+        {
+            itemname: 'potato',
+            Quantity: 1,
+            Quantityunits: 'kg',
+            price: 179
+        }
+    ],
+
+
+
+}
